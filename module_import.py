656a0f71addfc1adff06a0aa2e8f39fedaca8cf1
Kaestner Anders <anders.kaestner@psi.ch>
2022-03-01 08:17:28 +0100

    Updated environment. It works on a new anaconda installation (March 2022)

--- module_import.py
+++ module_import.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot
 import scipy 
 import mpl_toolkits.mplot3d
 import skimage
-# import plotsupport
 import sklearn
 import pandas
 import collections
@@ -12,25 +11,21 @@ import skimage.morphology
 import plotly.offline
 import seaborn
 import graphviz
-import pipe_utils
 import keras.models
 import bokeh
 import dask
 import tqdm.notebook
 import doctest
 import IPython.display
-import dask_ndfilters
-import dask_ndmorph
+import dask_image.ndfilters
+import dask_image.ndmorph
 import dask.array
 import dask.dot 
-import dask_ndfilters
 import functools
 import IPython.display
 import pydot
 import subprocess
 import tensorflow
-import affine_op
-import backport_ssim
 import sys
 import plotnine
 import itertools
